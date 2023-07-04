6a0ed964216c05a3ffd18216d65f320efccd9e6f
Ghazal masri <78875165+ghazal-masri@users.noreply.github.com>
2023-07-04 19:18:35 +0300

    login page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 
 <template>
+<page-login></page-login>
+ 
 
-  <h1 class="p-4 bg-gray-400">Hello world</h1>
-  
 
 
 </template>
 <script setup lang="ts">
+import pageLogin from "./components/page-login.vue";
 
 </script>
 
---
+++ src/components/page-login.vue
@@ -0,0 +1,27 @@
+<template>
+      <div class="bg-fl-secondary-300 h-screen w-screen flex">
+<div class="bg-white rounded-xl h-[30rem] w-[35rem] m-auto p-6 text-fl-primary-900 flex flex-col">
+<div class=" text-2xl font-semibold">Login</div>
+<div class="text-sm my-2">ENTER YOUR DETAILS BELOW TO CONTINUE</div>
+<div >   
+<input type="text" placeholder="Tenant ID" class="border-b-2 focus:outline-none focus:border-fl-primary-900 w-80 my-2 mx-3">
+<br>
+<input type="text" placeholder="Username" class="border-b-2 focus:outline-none focus:border-fl-primary-900 w-80 my-2 mx-3">
+<br>
+<input type="password" placeholder="Password" class="border-b-2 focus:outline-none focus:border-fl-primary-900 w-80 my-2 mx-3">  
+<br>
+<button type="button" class="hover:bg-fl-primary-100 bg-transparent font-semibold border border-fl-primary-900 rounded py-1 px-32 my-8 mx-5">LOG IN</button>
+</div>
+
+      <div class="flex justify-between mt-auto">
+      <div class="text-sm mt-auto">CONTACT SUPPORT</div>
+   <div> <img src="https://solarimpulse.com/files/companies/logo/2020-08-29-165550/logoFlowless.jpg" alt="flowless" class=" h-16 w-14 "></div> 
+   </div> 
+
+
+</div></div> 
+
+</template>
+<script  setup lang="ts">
+
+</script>
