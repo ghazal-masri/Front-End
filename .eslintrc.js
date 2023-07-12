85dd67fedfe56939261a774d018b6ac44a366839
Ghazal masri <78875165+ghazal-masri@users.noreply.github.com>
2023-07-12 18:08:03 +0300

    issue in Elsint file need to be fixed

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends:  [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'vue'],
+    rules: {
+       
+    },
+}
